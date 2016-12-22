b759ebbd43ebe6de2eb8ca6b98bac28a14cd6b69
WalkWithWind <sycf_ios_13579@sycf-iosdeiMac.local>
2016-12-22 09:00:51 +0800

    用Xcode8.2打开
    用Xcode8.2打开

--- NSCollectionView/ViewController.swift
+++ NSCollectionView/ViewController.swift
@@ -9,13 +9,19 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+   
+    @IBOutlet weak var collectionView: NSCollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.allowsMultipleSelection = true
+        collectionView?.register(NSNib.init(nibNamed: "SBItem", bundle: nil), forItemWithIdentifier: "SBItem")
+        
     }
-
+   
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
@@ -24,4 +30,20 @@ class ViewController: NSViewController {
 
 
 }
+extension ViewController: NSCollectionViewDelegate, NSCollectionViewDataSource,NSCollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    func numberOfSections(in collectionView: NSCollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItem(withIdentifier: "SBItem", for: indexPath) as? SBItem
+        item?.imgView.image = NSImage(named: indexPath.item.description)
+        print(indexPath.item.description)
+        item?.lab.stringValue = " NSTableRowView and custom NSView based NSTableview ? I initially started out with a view based NSTableView, but I soon noticed that I would have to handle the selection myself. I could not pull that off, so I went on to use NSTableRowView, which, strangely, does not call the initi"
+        return item!
+        
+    }
 
+}
